1d338cdb609faac0eee1deeff5b028031735bac0
Prince Patrick Anand Victor <princepatrickanand1996@gmail.com>
2023-10-08 08:01:21 -0400

    Revert "Adding project files"
    This reverts commit 6f03307e362389f7972934830dd30d9d8f49cb87.

--- src/main/java/com/example/mnmchatbotbackend/mnmrepositories/CaseFileRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.example.mnmchatbotbackend.mnmrepositories;
-//
-//import com.example.mnmchatbotbackend.mnmentities.CaseFile;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CaseFileRepository extends JpaRepository<CaseFile, Long> {
-//    // Add custom query methods if needed
-//}
